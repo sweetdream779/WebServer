d0acac4e69a2a53b2a976e6304570ca279cce87a
sweetdream779 <sweetdream779@gmail.com>
2017-11-26 00:51:25 +0300

    add img

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from flask import Flask, request, render_template, send_from_directory
 import urllib
+import numpy as np
+import cv2
 app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
     if request.method=='POST':
         #im = Image.open("images/bg.jpg")
-        with open('haarcascade_frontalface_default.xml','r') as f:
-            s='Yes'
-        return render_template("complete.html", str=s)
+        url='https://www.sciencenews.org/sites/default/files/2016/09/main/articles/090816_ls_brain-training_free.jpg'
+        resp = urllib.urlopen(url)
+        image = np.asarray(bytearray(resp.read()), dtype="uint8")
+        #image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+        return render_template("complete.html")
     return render_template("upload.html")
 
 if __name__ == '__main__':
