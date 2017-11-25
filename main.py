2765992944d18ea3bc8f65f6adfe98cee64b4169
sweetdream779 <sweetdream779@gmail.com>
2017-11-25 22:55:19 +0300

    add img

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def index():
 
 @app.route("/upload", methods=["POST"])
 def upload():
-    return render_template("complete.html")
+    return render_template("upload.html")
 
 if __name__ == '__main__':
   app.run()
