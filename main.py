ecc64c9d012cf5ddb1e624e7e1cf7fe4fa6a7321
sweetdream779 <sweetdream779@gmail.com>
2017-11-26 00:12:55 +0300

    add img

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from flask import Flask, request, render_template, send_from_directory
 import os
-from PIL import Image
 app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
     if request.method=='POST':
-        im = Image.open("images/bg.jpg")
+        #im = Image.open("images/bg.jpg")
+        os.mkdir('dir')
         return render_template("complete.html")
     return render_template("upload.html")
 
