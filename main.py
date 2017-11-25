5fcd299c8d94049d110853fb1484f34280db6cac
sweetdream779 <sweetdream779@gmail.com>
2017-11-26 00:54:49 +0300

    add img

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def index():
         #im = Image.open("images/bg.jpg")
         url='https://www.sciencenews.org/sites/default/files/2016/09/main/articles/090816_ls_brain-training_free.jpg'
         resp = urllib.urlopen(url)
-        image = np.asarray(bytearray(resp.read()), dtype="uint8")
+        #image = np.asarray(bytearray(resp.read()), dtype="uint8")
         #image = cv2.imdecode(image, cv2.IMREAD_COLOR)
         return render_template("complete.html")
     return render_template("upload.html")
