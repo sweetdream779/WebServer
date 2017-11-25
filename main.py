cca726a9d194dc501efc9fab9d8cb7cc1fda513d
sweetdream779 <sweetdream779@gmail.com>
2017-11-25 22:17:08 +0300

    add img

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, request, render_template, send_from_directory
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-  return 'Hello, World!'
+@app.route("/")
+def index():
+    return render_template("upload.html")
+
+@app.route('/upload/<filename>')
+def send_image(filename):
+    return send_from_directory("images", filename)
 
 if __name__ == '__main__':
   app.run()
