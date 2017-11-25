fd52715dfd621671cd262e0aa6406169516a82f2
sweetdream779 <sweetdream779@gmail.com>
2017-11-25 23:21:15 +0300

    add img

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("upload.html")
 
-@app.route("/upload", methods=['GET',"POST"])
+@app.route("/complete", methods=["POST"])
 def upload():
     return render_template("complete.html")
 
 if __name__ == '__main__':
-  app.run()
+  app.run(debug=True)
