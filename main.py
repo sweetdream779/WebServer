10ea036408409e4012ccf3e17fc8f862d9dae40a
sweetdream779 <sweetdream779@gmail.com>
2017-11-26 14:07:54 +0300

    add text

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 from flask import Flask, request, render_template, send_from_directory
-import urllib
-import numpy as np
 app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
     if request.method=='POST':
-        url='https://www.sciencenews.org/sites/default/files/2016/09/main/articles/090816_ls_brain-training_free.jpg'
-        #resp = urllib.urlopen(url)
-        #image = np.asarray(bytearray(resp.read()), dtype="uint8")
-        #image = cv2.imdecode(image, cv2.IMREAD_COLOR)
-        return render_template("complete.html")
+        #upload=request.files.getlist("file")[0]
+        f = request.files['file'][0]
+        t=f.read()
+
+        return render_template("complete.html", text=t)
     return render_template("upload.html")
 
 if __name__ == '__main__':
