c6ae36be6f2ccc62d7ca0415a38ab2b6dc4d5c2f
sweetdream779 <sweetdream779@gmail.com>
2017-11-26 15:47:39 +0300

    add words, lines count

--- main.py
+++ main.py
@@ -3,24 +3,33 @@ from flask import Flask, request, render_template, send_from_directory
 app = Flask(__name__)
 
 def count_words(file):
-    pass
+    i=0
+    for line in f:
+        s=f.readline()[:-1]
+        words=s.split(' ')
+        i+=len(words)
+    return i
 
 
 def count_chars(file):
     pass
 
 
+def count_lines(file):
+    i=0
+    for line in f:
+        s=f.readline()
+        i+=1
+    return i
+
+
 @app.route("/",methods=["GET","POST"])
 def index():
     if request.method=='POST':
-        f = request.files['file']
-        i=0
-        for line in f:
-            s=f.readline()[2:-1]
-            i+=1
-            if 2==1:
-                break
-        return render_template("complete.html",text=s)
+        file = request.files['file']
+        lines=str(count_lines(file))
+        words=str(count_words(file))
+        return render_template("complete.html",lines=lines,words=words)
     return render_template("upload.html")
 
 if __name__ == '__main__':
