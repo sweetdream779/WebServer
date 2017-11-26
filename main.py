8a1c72da67c8e0c292f30d94c5fcdcf8659370c0
sweetdream779 <sweetdream779@gmail.com>
2017-11-26 14:20:14 +0300

    add text

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ app = Flask(__name__)
 @app.route("/",methods=["GET","POST"])
 def index():
     if request.method=='POST':
-        f = request.files['file'][0]
-        t=f.read()
         return render_template("complete.html", text=t)
     return render_template("upload.html")
 
