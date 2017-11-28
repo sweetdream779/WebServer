8f1a2f35daec64aba3d83fca2be913656f8cf6b2
Irina Krasnova <sweetdream779@gmail.com>
2017-11-28 12:50:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ def count_words_chars_lines(file):
     lines_count=0
     words_count=0
     chars_count=0
+    existWords=[]
+    str_words=""
     for line in file:
         line=line.decode()
         if(len(line.split('\n'))>1):
@@ -13,16 +15,23 @@ def count_words_chars_lines(file):
         else:
             line=line.split('\r\n')[0]
         chars_count+=len(line)
-        words_count+=wordCount(line)
+        words_count+=wordCount(line,existWords)
+        str_words.join(existWords)
         lines_count+=1
-    return [str(words_count), str(chars_count), str(lines_count)]
+    
+    return [str(words_count), str(chars_count), str(lines_count), str_words]
 
 
-def wordCount(line):
+def wordCount(line,existWords):
     words=line.split(' ')
     l=len(words)
     if len(line)>0 and len(words)==0:
         l=1
+        existsWords.append(words)
+    else:
+        for word in words:
+            if(not word in existsWords):
+                existsWords.append(word)
     return l
 
 
@@ -32,7 +41,7 @@ def index():
     if request.method=='POST':
         file = request.files['file']
         result=count_words_chars_lines(file)
-        return render_template("upload.html",notvisibility=1,words=result[0], chars=result[1], lines=result[2])
+        return render_template("upload.html",notvisibility=1,words=result[0], chars=result[1], lines=result[2], unikWords=result[3])
     return render_template("upload.html", notvisibility=0)
 
 if __name__ == '__main__':
