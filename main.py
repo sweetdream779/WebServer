456bd8d21884b6219957ea940a36ca90270b23d6
sweetdream779 <sweetdream779@gmail.com>
2017-11-29 03:17:39 +0300

    add another function

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, send_from_directory
+from flask import Flask, request, render_template, send_from_directory, session, url_for, redirect
 
 app = Flask(__name__)
 
@@ -25,27 +25,75 @@ def wordCount(line):
         l=1
     return l
 
+def get_unique_words_from_file(file):
+    words1=[]
+    for line in file:
+        line=line.decode()
+        if(len(line.split('\n'))>1):
+            line=line.split('\n')[0]
+        else:
+            line=line.split('\r\n')[0]
+        
+        words=line.split(' ')
+        if len(line)>0 and len(words)==0 and words not in words1:
+            words1.append(words)
+            continue
+        for word in words:
+            if word not in words1:
+                words1.append(word)
+    return words1
+
+def count_differences(file1,file2):
+    distance=0
+    similar=0
+    words1=get_unique_words_from_file(file1)
+    words2=get_unique_words_from_file(file2)
+    dist=[]
+    for word in words1:
+        if word not in words2:
+            distance+=1
+            dist.append(word)
+        else:
+            similar+=1
+    for word in words2:
+        if word not in words1 and word not in dist:
+            dist.append(word)
+            distance+=1
+    return [str(distance), len(words1), len(words2), str(similar)]
+
 
 
 @app.route("/")
 def index():
         return render_template("index.html")
 
-@app.route("/compare",methods=["POST"])
+@app.route("/compare",methods=["GET","POST"])
 def compare():
-    if request.method=='POST':
-        file = request.files['file']
-        result=count_words_chars_lines(file)
-        return render_template("upload.html",notvisibility=1,words=result[0], chars=result[1], lines=result[2])
-return render_template("upload.html", notvisibility=0)
+    return render_template("compare.html", notvisibility=0)
 
-@app.route("/count",methods=["POST"])
+@app.route("/compare/result",methods=["POST"])
+def compare_res():
+    if(request.files['file1'].filename!='' and request.files['file2'].filename!=""):
+        file1 = request.files['file1']
+        file2 = request.files['file2']
+        result=count_differences(file1,file2)
+        return render_template("compare.html",notvisibility=1,distance=result[0], words1=result[1], words2=result[2], similar=result[3])
+    else:
+        return redirect(url_for('compare'))
+
+@app.route("/count",methods=["GET","POST"])
 def count():
-    if request.method=='POST':
+    return render_template("count.html",notvisibility=0)
+
+@app.route("/count/result",methods=["POST"])
+def count_res():
+    if(request.files['file'].filename!=""):
+        print(request.files['file'].filename)
         file = request.files['file']
         result=count_words_chars_lines(file)
-        return render_template("upload.html",notvisibility=1,words=result[0], chars=result[1], lines=result[2])
-return render_template("upload.html", notvisibility=0)
+        return render_template("count.html",notvisibility=1,words=result[0], chars=result[1], lines=result[2])
+    else:
+        return redirect(url_for('count'))
 
 if __name__ == '__main__':
   app.run()
--- test_web.py
+++ test_web.py
@@ -1,4 +1,4 @@
-from flask import Flask, Request, request
+from flask import Flask, Request, request, url_for
 import unittest
 from io import BytesIO
 
@@ -15,16 +15,62 @@ class FlaskTest(unittest.TestCase):
         result = self.app.get('/')
         self.assertEqual(result.status_code, 200)
 
-    def test_post_request(self):
+    def test_compare_status_code(self):
+        result = self.app.get('/compare')
+        self.assertEqual(result.status_code, 200)
+
+    def test_count_status_code(self):
+        result = self.app.get('/count')
+        self.assertEqual(result.status_code, 200)
+
+
+    def test_post_count_request(self):
         """Assert that user is redirected with status 302 after creating a todo item"""
-        '''data = dict(
+        data = dict(
             file=(BytesIO(b'my file contents'), "file.txt")
         )
-        response = self.app.post('/', content_type='multipart/form-data',
+        response = self.app.post('/count/result', content_type='multipart/form-data',
                                  data=data
                                  )
-        self.assertEqual(response.status_code, 200)'''
-        pass
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_count_one_request(self):
+        """Assert that user is redirected with status 302 after creating a todo item"""
+        data = dict(
+            file=(BytesIO(b'contents'), "file.txt")
+        )
+        response = self.app.post('/count/result', content_type='multipart/form-data',
+                                 data=data
+                                 )
+        self.assertEqual(response.status_code, 200)
+
+    def test_empty_post_count_request(self):
+        """Assert that user is redirected with status 302 after creating a todo item"""
+
+        response = self.app.post('/count/result', content_type='multipart/form-data',
+                                 data="", follow_redirects=True
+                                 )
+        
+    def test_post_compare_request(self):
+        """Assert that user is redirected with status 302 after creating a todo item"""
+        data = dict(
+            file1=(BytesIO(b'my file contents'), "file1.txt"),
+            file2=(BytesIO(b'my file'), "file2.txt")
+        )
+        response = self.app.post('/compare/result', content_type='multipart/form-data',
+                                 data=data
+                                 )
+        self.assertEqual(response.status_code, 200)
+
+    def test_empty_post_compare_request(self):
+        """Assert that user is redirected with status 302 after creating a todo item"""
+        data = dict(
+            file1="",
+            file2=""
+        )
+        response = self.app.post('/compare/result', content_type='multipart/form-data',
+                                 data=data, follow_redirects=True
+                                 )
 
 if __name__ == '__main__':
     unittest.main()
